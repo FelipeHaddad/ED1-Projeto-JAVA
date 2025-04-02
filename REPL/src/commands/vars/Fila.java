ed96433507df1edefbf05e99d79e0f055c4afed8
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-04-02 18:54:20 -0300

    Update Fila.java

--- REPL/src/commands/vars/Fila.java
+++ REPL/src/commands/vars/Fila.java
@@ -1,84 +1,90 @@
 /* 1° Felipe Bertacco Haddad - 10437372
  * 2° Ana Julia Yaguti - 10436655
  * 3° Beatriz Nobrega - 10435789
+*/
 
- */
+package commands.vars;
 
- package commands.vars;
+public class Fila <T> {
 
- public class Fila <T> {
- 
-     private T[] fila;
-     private int ultPosicao;
-     private int priPosicao;
- 
-     public Fila() {
-         this.fila = (T[]) new Object[10];
-         this.ultPosicao = 0;
-         this.priPosicao = 0;
-     }
- 
-     public boolean qIsFull() {
-         if (this.ultPosicao == this.fila.length ) {
-             return true;
-         } else {
-             return false;
-         }
-     }
- 
-     public boolean qIsEmpty() {
-         if (this.ultPosicao == 0) {
-             return true;
-         } else {
-             return false;
-         }
-     }
- 
-     public void enqueue (T element) {
-         if (!qIsFull()) {
-             fila[ultPosicao] = element;
-             ultPosicao++;
-         } else {
-             System.out.print("Fila cheia!");
-         }
-     }
- 
-     public T dequeue () {
-         T aux = null;
-         fila[priPosicao] = aux;
-         fila[priPosicao] = null;
-         priPosicao--;
-         return aux;
-         }
- 
-     public T rear () {
-         return fila[priPosicao];
-     }
- 
-     public T front() {
-         return fila[ultPosicao - 1];
-     }
- 
-     public int size () {
-         int count = 0;
-         for (int i = 0; i < fila.length; i++) {
-             if (fila[i] != null) {
-                 count++;
-             }
-         }
-         return count;
-     }
- 
-     public void printAllElements () {
-         System.out.println("[");
-         for (int j = 0; j < 10; j++) {
-             System.out.print(fila[j] + ",");
-         } System.out.println("]");
-     }
- 
- 
-     public static void main (String[] args) {
- 
-     }
- }
- 
+    private T[] fila;
+    private int ultPosicao;
+    private int priPosicao;
+
+    public Fila() {
+        this.fila = (T[]) new Object[10];
+        this.ultPosicao = 0;
+        this.priPosicao = 0;
+    }
+
+    public boolean qIsFull() {
+        if (this.ultPosicao == this.fila.length ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean qIsEmpty() {
+        if (this.ultPosicao == this.priPosicao) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void enqueue (T element) {
+        if (!qIsFull()) {
+            fila[ultPosicao] = element;
+            ultPosicao++;
+        } else {
+            System.out.print("Fila cheia!");
+        }
+    }
+
+    public T dequeue () {
+        T aux;
+        aux = fila[priPosicao];
+        fila[priPosicao] = null;
+        priPosicao++;
+        return aux;
+        }
+
+    public T front () {
+        return fila[priPosicao];
+    }
+
+    public T rear() {
+        return fila[ultPosicao - 1];
+    }
+
+    public int size () {
+        int count = 0;
+        for (int i = 0; i < fila.length; i++) {
+            if (fila[i] != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getPriPosicao () {
+        return this.priPosicao;
+    }
+
+    public int getUltPosicao () {
+        return this.ultPosicao;
+    }
+
+    public void printAllElements () {
+        System.out.print("[");
+        for (int j = 0; j < 10; j++) {
+            System.out.print(fila[j] + ",");
+        } System.out.println("]");
+    }
+
+
+    public static void main (String[] args) {
+
+    }
+}
